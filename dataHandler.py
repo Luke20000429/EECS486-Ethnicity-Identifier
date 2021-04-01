5aeea4f0c1c8e568bbce81a8ff08f0930cff9dde
liuxueshen429 <1151820475@qq.com>
2021-04-01 15:31:53 +0800

    bayes

--- dataHandler.py
+++ dataHandler.py
@@ -8,15 +8,16 @@ def readData(regionINFO='data/regions.txt', nameINFO='data/redb.txt'):
 
     with open(regionINFO, 'r', encoding='utf-8') as f:
         for line in f:
-            region = line[:-1]
-            regions.append(region) ## #line:region_id region
+            region = line[:-1].split(" ")
+            region[1] = int(region[1])
+            regions.append(region) ## #line:region_id num_of_data
             
 
     with open(nameINFO, 'r', encoding='utf-8') as f:
         for line in f:
-            na_na = line[:-1].split(" ")
-            na_na[1] = int(na_na[1])
-            name_pairs.append(tuple(na_na)) ## name region_id
+            na_re = line[:-1].split(" ")
+            na_re[1] = int(na_re[1])
+            name_pairs.append(tuple(na_re)) ## name region_id
 
     return regions, name_pairs
 
--- ngram.py
+++ ngram.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 def getNgrams(N, pairs, regions):
+    ## get data of uni/bi/tri-grams
     ngrams = [dict() for i in regions]
     N = N-1
     for name, rid in pairs:
@@ -13,3 +14,18 @@ def getNgrams(N, pairs, regions):
                 ngrams[rid][gram] = 1
 
     return ngrams
+
+def getGrams(name):
+    ## split a name into uni/bi/tri-grams
+    grams = []
+    for N in range(3):
+        grams.append([])
+        for i in range(N, len(name)):
+            grams[N].append(name[i-N:i+1])
+
+    return grams
+
+
+if __name__ == '__main__':
+    print(getGrams("liuxueshen"))
+    
